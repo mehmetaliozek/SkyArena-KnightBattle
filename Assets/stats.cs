26b78cc6b3296fd745db7b973b721705aa87083d
Doğukan <dogukanozek@gmail.com>
2023-02-24 16:51:44 +0300

    statlar eklendi

---
+++ Assets/stats.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stats : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]private float attack;
+    [SerializeField]private float defense;
+    [SerializeField]private float gold;
+    [SerializeField]private float speed;
+    [SerializeField]private float health;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
