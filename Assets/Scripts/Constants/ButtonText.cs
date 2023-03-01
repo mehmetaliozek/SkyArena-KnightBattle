c38346cb8907fb844aa165b1829842da87fee582
Mehmet Ali Özek <mehmetaliozek761@gmail.com>
2023-03-01 16:23:24 +0300

    Sayfalar arası geçiş düzenlendi

---
+++ Assets/Scripts/Constants/ButtonText.cs
@@ -0,0 +1,7 @@
+public class ButtonText
+{
+    public const string play = "Play";
+    public const string options = "Options";
+    public const string exit = "Exit";
+
+}
---
+++ Assets/Scripts/UI/Game.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Game : MonoBehaviour, IPointerEvents
+{
+    public void PointerDown(GameObject btn)
+    {
+    }
+
+    public void PointerUp(GameObject btn)
+    {
+    }
+}
