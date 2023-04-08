f562cdf5754ea4bc5b9f81bbc82402191893ae04
Doğukan <dogukanozek@gmail.com>
2023-04-08 16:30:05 +0300

    Müzik ve Ses Effekti (v1) Eklendi

---
+++ Assets/Auido.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Auido : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //Ses ve Müzik İçin game objectler
+    public GameObject auido;
+    public GameObject auido1;
+    //Slidera bağlı fonksiyon müzik için
+    public void AuidoChange(float value){
+        auido.GetComponent<AudioSource>().volume=value;
+    }
+    //Slidera bağlı fonkisyon ses efekti için
+    public void AuidoChange1(float value){
+        auido1.GetComponent<AudioSource>().volume=value;
+    }
+    //Ses Efektini oynatma fonksiyonu
+    public void AuidoEffectPlay(){
+        auido1.GetComponent<AudioSource>().Play();
+    }
+}
